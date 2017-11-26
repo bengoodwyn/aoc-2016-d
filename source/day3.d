08294f8021674af4bd7bc657044e3210ed32aee1
Ben Goodwyn <ben@clouddirective.com>
2017-11-26 12:44:33 -0600

    :new: Day 3 Part 2

--- source/day3.d
+++ source/day3.d
@@ -17,3 +17,34 @@ unittest {
     assert(1 == part1(["5 21 25"]));
     assert(1 == part1(["25 21 5"]));
 }
+
+private int[] group9(int[] accum, int x) {
+    if (accum.length == 9) {
+        return [x];
+    } else {
+        return accum ~ x;
+    }
+}
+
+private auto adapt(T)(T lines) {
+    int[] seed;
+    return lines
+            .map!split
+            .joiner
+            .map!(x => to!int(x))
+            .cumulativeFold!(group9)(seed)
+            .filter!(x => x.length == 9)
+            .map!(x => [
+                    "%d %d %d".format(x[0],x[3],x[6]),
+                    "%d %d %d".format(x[1],x[4],x[7]),
+                    "%d %d %d".format(x[2],x[5],x[8])
+                ])
+            .joiner;
+}
+
+auto part2(T)(T lines) {
+    auto vertical_lines = adapt(lines);
+
+    return part1(vertical_lines);
+}
+
