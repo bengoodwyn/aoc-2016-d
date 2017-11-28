900f75eb2768c4130f89b9ffab149f2509ac27e9
Ben Goodwyn <ben@clouddirective.com>
2017-11-28 12:25:51 -0600

    :new: Day 12 Part 2

--- source/day12.d
+++ source/day12.d
@@ -127,6 +127,11 @@ int part1(T)(T code) {
     return code.compile_instructions.execute[resolve_register("a")];
 }
 
+int part2(T)(T code) {
+    auto modified_code = ["cpy 1 c"].chain(code);
+    return part1(modified_code);
+}
+
 unittest {
     immutable sample_code =
     [
